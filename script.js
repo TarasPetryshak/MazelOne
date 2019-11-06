521b48c617569e262950f5085f6e0a829b04080b
Taras Petryshak <petryshak.taras@gmail.com>
2019-11-06 17:12:44 +0200

    add second slider

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
-$('.owl-carousel').owlCarousel({
-    rtl:false,
-    margin:10,
-    nav: true,
-    dots:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
+$(document).ready(function () {$('#carousel1').owlCarousel({
+        rtl:false,
+        margin:20,
+        nav: false,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            767:{
+                items:2
+            },
+            991:{
+                items:3
+            },
+            1200:{
+                items:4
+            }
         }
-    }
+    })
 })
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
-});
-let projects = {
-    1: 'project-1',
-    2: 'project-2',
-    3: 'project-3',
-    4: 'project-4',
-    5: 'project-5',
-    6: 'project-6'
-};
-let galleryPhotos = {
-    1: 'photo-gallery-1',
-    2: 'photo-gallery-2',
-    3: 'photo-gallery-3',
-    4: 'photo-gallery-4',
-    5: 'photo-gallery-5',
-    6:' photo-gallery-6'
-}
-$('.menu-slider').click(() => {
-    for (let id in projects) {
-        if ($(event.target).hasClass(projects[id])) {
-            $('.gallery-images').find(`#${galleryPhotos[id]}`).addClass('fullScreen');
-            $('section.gallery').css({'top':'0'});
-        }
-    }
+$(document).ready(function () {$('#carousel2').owlCarousel({
+        rtl:false,
+        nav: false,
+        dots:true,
+        loop: true,
+        items: 1,
+        autoplay:true,
+        smartSpeed:1000,
+        autoplayTimeout:3000
+    })
 })
