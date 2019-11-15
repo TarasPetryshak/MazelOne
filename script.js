a6781a4a6adedc319d87c1b1ffdb7aec2de3a96f
Taras <petryshak.taras@gmail.com>
2019-11-15 20:21:55 +0200

    add breakpoint 992px and counters

--- script.js
+++ script.js
@@ -15,9 +15,6 @@ $(document).ready(function() {
                 items: 2
             },
             1200: {
-                items: 3
-            },
-            1400: {
                 items: 4
             }
         }
@@ -44,7 +41,7 @@ $(document).ready(function() {
         items: 1,
         autoplay: true,
         smartSpeed: 1000,
-        autoplayTimeout: 4000
+        autoplayTimeout: 3000
     })
 })
 $(document).ready(function() {
@@ -60,9 +57,9 @@ $(document).ready(function() {
         autoplayTimeout: 2000,
         responsive: {
             0: {
-                items: 1
+                items: 3
             },
-            767: {
+            544: {
                 items: 3
             },
             991: {
@@ -83,40 +80,74 @@ $(document).ready(function() {
         items: 1,
         autoplay: true,
         smartSpeed: 1000,
-        autoplayTimeout: 4000
+        autoplayTimeout: 3000
     })
 })
+let firstCounterElement = $('#counter-1');
+let counterPosition = (elem) => elem[0].getBoundingClientRect().top - $(window).height() + 50;
+let flag = true;
+let drawLine = () => {
+    if (flag) {
+        let line = $(".skills__block_element-inner");
+        let counters = $(".count");
+        let countersQuantity = counters.length;
+        let counter = [];
+        for (let i = 0; i < countersQuantity; i++) {
+            counter[i] = parseInt(counters[i].innerHTML);
+        }
+        let count = (start, value, id) => {
+            let localStart = start;
+            setInterval(function() {
+                if (localStart < value) {
+                    localStart++;
+                    line[id].style.width = `${localStart}%`;
+                }
+            }, 35);
+        }
+        for (let j = 0; j < countersQuantity; j++) {
+            count(0, counter[j], j);
+        }
+    }
+    flag = false;
+};
 
-/*$(document).ready(function() {
 
-  var counters = $(".count");
-  var countersQuantity = counters.length;
-  var counter = [];
+$(document).scroll(() => {
+    if (counterPosition(firstCounterElement) < 0) {
+        drawLine();
+    }
+});
+let secondCounterElement = $('#counter-2');
+let flag2 = true;
+let setNumber = () => {
+    if (flag2) {
+        let counters = $(".count-2");
+        let countersQuantity = counters.length;
+        let counter = [];
+        for (let i = 0; i < countersQuantity; i++) {
+            counter[i] = parseInt(counters[i].innerHTML);
+        }
+        let count = (start, value, id) => {
+            let localStart = start;
+            setInterval(function() {
+                if (localStart < value) {
+                    localStart++;
+                    counters[id].innerHTML = localStart;
+                }
+            }, 10);
+        }
+        for (let j = 0; j < countersQuantity; j++) {
+            count(0, counter[j], j);
+        }
+    }
+    flag2 = false;
+};
+$(document).scroll(() => {
+    if (counterPosition(secondCounterElement) < 0) {
+        setNumber();
+    }
+})
 
-  for (i = 0; i < countersQuantity; i++) {
-    counter[i] = parseInt(counters[i].innerHTML);
-  }
-
-  var count = function(start, value, id) {
-    var localStart = start;
-    setInterval(function() {
-      if (localStart < value) {
-        localStart++;
-        counters[id].innerHTML = localStart;
-      }
-    }, 40);
-  }
-
-  for (j = 0; j < countersQuantity; j++) {
-    count(0, counter[j], j);
-  }
-});*/
-
-/*<div class="count">45</div> 
-<div class="count">100</div> 
-<div class="count">115</div> 
-<div class="count">50</div>
-<div class="count">125</div>*/
 let elementsBlocks = [
     'video',
     'slider',
@@ -128,14 +159,14 @@ $('.elements__nav_box').click(() => {
     event.preventDefault();
     let target = event.target.id;
     for (let act of elementsBlocks) {
-        if (target == act)
+        if (target == act) {
             for (let act of elementsBlocks) {
                 if (target == act) {
                     $(`.${target}`).removeClass('elements__block-hidden');
                 } else {
                     $(`.${act}`).addClass('elements__block-hidden');
                 }
-
             }
+        }
     }
 })
